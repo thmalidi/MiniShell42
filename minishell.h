64062bf1faca410b129daf50e8edb70847adb44b
tmalidi <theomalidiorientation@gmail.com>
2023-06-26 11:24:19 +0200

    init

--- minishell.h
+++ minishell.h
@@ -6,9 +6,11 @@
 /*   By: tmalidi <tmalidi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/26 11:04:22 by tmalidi           #+#    #+#             */
-/*   Updated: 2023/06/26 11:05:25 by tmalidi          ###   ########.fr       */
+/*   Updated: 2023/06/26 11:23:52 by tmalidi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+//hgeffroy
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
