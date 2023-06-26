7f15b046751e509e24e218c091831d7875ca5579
tmalidi <theomalidiorientation@gmail.com>
2023-06-26 11:22:12 +0200

    save

--- minishell.h
+++ minishell.h
@@ -6,9 +6,11 @@
 /*   By: tmalidi <tmalidi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/26 11:04:22 by tmalidi           #+#    #+#             */
-/*   Updated: 2023/06/26 11:13:23 by tmalidi          ###   ########.fr       */
+/*   Updated: 2023/06/26 11:21:50 by tmalidi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+//tmalidi
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
