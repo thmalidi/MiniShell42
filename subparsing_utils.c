b40429135791590ff4ec5fa078fa9d49fba7c860
tmalidi <theomalidiorientation@gmail.com>
2023-07-17 12:49:31 +0200

    subparsing normalement c ok

--- subparsing_utils.c
+++ subparsing_utils.c
@@ -6,7 +6,7 @@
 /*   By: tmalidi <tmalidi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/16 09:15:36 by tmalidi           #+#    #+#             */
-/*   Updated: 2023/07/16 11:12:00 by tmalidi          ###   ########.fr       */
+/*   Updated: 2023/07/17 12:40:57 by tmalidi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,7 @@ int is_cmd(char **path, char *cmd)
 	{
 		joined = ft_strjoin(*path, tmp);
 		if (access(joined, X_OK) == 0)
-			return (free(tmp), 1);
+			return (free(tmp), free(joined), 1);
 		free(joined);
 		path++;
 	}
@@ -46,15 +46,21 @@ int    subparsing(t_element **subparsing)
 	envp = ft_split(getenv("PATH"), ':');
     while (tmp)
     {
-        if (!ft_strncmp("<",tmp->str, ft_strlen(tmp->str)))
+		printf("tmp[0] == %c\n",tmp->str[0]);
+        if (!ft_strncmp("<",tmp->str, ft_strlen(tmp->str)))																			//infile
             tmp->type = 3;
-        else if (!strncmp(">",tmp->str, ft_strlen(tmp->str)) && i == 1)
+        else if (!strncmp(">",tmp->str, ft_strlen(tmp->str)) && i == 1)																//outfile
             tmp->type = 4;
-		else if ((i == 0 || (tmp->next == NULL && first->type == 3)) && is_cmd(envp, tmp->str))
-			tmp->type = 2;
-		else if (tmp->previous)
+		else if(tmp->str[0] == '-')																									//flag
 		{
-        	if ((tmp->previous->type == 3 || tmp->previous->type == 4 || tmp->previous->type == 2) && !access(tmp->str, F_OK))
+			if (tmp->previous->type == 2 || tmp->previous->type == 5)
+				tmp->type = 5;
+		}
+		else if ((i == 0 || (tmp->next == NULL && first->type == 3)) && is_cmd(envp, tmp->str))										//commande
+			tmp->type = 2;
+		else if (tmp->previous)																										//fichier
+		{
+        	if ((tmp->previous->type == 3 || tmp->previous->type == 4 || tmp->previous->type == 2 || tmp->previous->type == 5) && !access(tmp->str, F_OK))
 				tmp->type = 1;
 		}
 		else
