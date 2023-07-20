88340d1f3e34b53faa84748060be9a559c450147
Henri Geffroy <hgeffroy@z4r8p5.42lyon.fr>
2023-07-20 07:46:04 +0200

    Rien

--- builtins/utils.c
+++ builtins/utils.c
@@ -1,20 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   minishell.h                                        :+:      :+:    :+:   */
+/*   utils.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: hgeffroy <hgeffroy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/06/26 11:04:22 by tmalidi           #+#    #+#             */
-/*   Updated: 2023/07/16 16:18:45 by hgeffroy         ###   ########.fr       */
+/*   Created: 2023/07/19 14:09:31 by hgeffroy          #+#    #+#             */
+/*   Updated: 2023/07/19 14:25:02 by hgeffroy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-//hgeffroy
+//Pas sur que ce soit utile
+int	is_var(char	*var)
+{
+	
+}
 
-//Probbalement a remplir vers la fin du projet, pour le moment on peut travailler avec nos .h chacun et on mettra en commun vers la fin ?
 
-#ifndef MINISHELL_H
-# define MINISHELL_H
-
-#endif
+int	set_value(char *var, char *value)
+{
+	
+}
