cb2c25fc6931c85628c2eabd93dbf664e1ec1b12
Henri Geffroy <hgeffroy@z4r8p5.42lyon.fr>
2023-07-30 08:05:43 +0200

    test

--- source/builtins/env.c
+++ source/builtins/env.c
@@ -6,7 +6,7 @@
 /*   By: hgeffroy <hgeffroy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/20 08:13:23 by hgeffroy          #+#    #+#             */
-/*   Updated: 2023/07/20 12:56:41 by hgeffroy         ###   ########.fr       */
+/*   Updated: 2023/07/30 08:05:32 by hgeffroy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,3 +20,5 @@ int	env(t_pipelist *pipelist, char **env)
 	print_tab(env);
 	return (0);
 }
+
+//Test
