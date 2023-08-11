d4cbbebc423c1cbadecc0456ed511b69f43fb951
Hgeffroy <henri.geffroy@gmail.com>
2023-08-11 18:47:02 +0200

    exit

--- source/builtins/exit.c
+++ source/builtins/exit.c
@@ -6,13 +6,15 @@
 /*   By: hgeffroy <hgeffroy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/06 09:04:16 by hgeffroy          #+#    #+#             */
-/*   Updated: 2023/08/11 14:49:01 by hgeffroy         ###   ########.fr       */
+/*   Updated: 2023/08/11 18:46:00 by hgeffroy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int	exit_b(void)
+int	exit_b(t_datalist *data, t_env **env)
 {
+	(void)data;
+	(void)env;
 	return (0);
 }
