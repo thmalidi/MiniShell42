ce7a365c0d60b365e745896553992ab883af7f65
tmalidi <theomalidiorientation@gmail.com>
2023-08-15 18:04:18 +0200

    impl ok

--- test_for_compa/source/parsing/subparsing.c
+++ test_for_compa/source/parsing/subparsing.c
@@ -6,7 +6,7 @@
 /*   By: tmalidi <tmalidi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/14 12:59:50 by tmalidi           #+#    #+#             */
-/*   Updated: 2023/08/15 09:23:21 by tmalidi          ###   ########.fr       */
+/*   Updated: 2023/08/15 18:03:49 by tmalidi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,7 +40,13 @@ void	splited_arg(t_big_list *arg)
 		*tmp->pipelist = NULL;
 		tab = ft_split(tmp->content, ' ');
 		while (tab[i])
-			ft_lstadd_back_e(tmp->pipelist, ft_lstnew_e(tab[i++]));
+		{
+			tab[i] = put_space(tab[i]);
+			i++;
+		}
+		i = 0;
+		while (tab[i])
+			ft_lstadd_back_e(tmp->pipelist, ft_lstnew_e(tab[i]));
 		free_tab(tab);
 		tmp = tmp->next;
 	}
