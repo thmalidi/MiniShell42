dc6684aebc9e1467e2ff16e0823815a76724de12
hgeffroy <henri.geffroy@gmail.com>
2023-09-01 19:31:50 +0200

    Corrections de cat avec file inexistant

--- source/exec/exec.c
+++ source/exec/exec.c
@@ -1,4 +1,4 @@
-/* ************************************************************************** */
+/******************************************************************************/
 /*                                                                            */
 /*                                                        :::      ::::::::   */
 /*   exec.c                                             :+:      :+:    :+:   */
@@ -6,9 +6,9 @@
 /*   By: hgeffroy <hgeffroy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/26 13:48:55 by hgeffroy          #+#    #+#             */
-/*   Updated: 2023/08/31 16:34:44 by hgeffroy         ###   ########.fr       */
+/*   Updated: 2023/09/01 17:24:58 by hgeffroy         ###   ########.fr       */
 /*                                                                            */
-/* ************************************************************************** */
+/******************************************************************************/
 
 #include "minishell.h"
 
@@ -23,12 +23,14 @@ int	set_pipe(t_datalist *list, int *fd)
 {	
 	fd[0] = fd[2];
 	fd[1] = fd[3];
+	fd[2] = 0;
 	fd[3] = 0;
 	if (list->next)
 	{
 		if (pipe(&fd[2]) < 0)
 			return (-1);
 	}
+	//dprintf(2, "set pipe:\n%d\n%d\n%d\n%d\n", fd[0], fd[1], fd[2], fd[3]);
 	return (0);
 }
 
@@ -39,13 +41,13 @@ On doit prioriser les infile et outfile aux pipes.
 */
 int	set_dup(t_datalist *list, int *fd)
 {
-	if (list->infile)
+	if (list->infile > 0)
 		dup2(list->infile, STDIN_FILENO);
-	else if (fd[0])
+	else if (fd[0] > 0)
 		dup2(fd[0], STDIN_FILENO);
-	if (list->outfile)
+	if (list->outfile > 0)
 		dup2(list->outfile, STDOUT_FILENO);
-	else if (fd[3])
+	else if (fd[3] > 0)
 		dup2(fd[3], STDOUT_FILENO);
 	close_fd(fd, 4);
 	return (0);
@@ -61,6 +63,23 @@ void	exec_builtin(t_datalist *datalist, t_env **envlst, int builtin)
 
 
 	(*tab_builtins[builtin])(datalist, envlst);
+}
+
+int exec_nobuiltin(t_datalist *datalist, t_env **envlst)
+{
+	char	*cmdwpath;
+	char	**env;
+
+	signal(SIGQUIT, &child_handler);
+	env = env_to_tab(*envlst); // A free, ca malloc + protection
+	cmdwpath = check_cmd(env, datalist->cmd); // A free
+	if (!cmdwpath)
+	{
+		free_tab(env);
+		exit (-1);
+	}
+	execve(cmdwpath, datalist->args, env);
+	return (0);
 }
 
 /*
@@ -93,8 +112,8 @@ il faudra check que le perror renvoie bien les bons trucs.
 */
 int	exec_onepipe(t_datalist *datalist, int *fd, t_env **envlst)
 {	
-	char	*cmdwpath;
-	char	**env;
+	// char	*cmdwpath;
+	// char	**env;
 	int		builtin;
 
 	
@@ -104,6 +123,8 @@ int	exec_onepipe(t_datalist *datalist, int *fd, t_env **envlst)
 	else
 	{
 		datalist->pid = fork();
+		if ((datalist->infile < 0 || datalist->outfile < 0) && datalist->pid == 0)
+			exit (1);
 		if (datalist->pid == 0)
 		{
 			set_dup(datalist, fd);
@@ -114,15 +135,16 @@ int	exec_onepipe(t_datalist *datalist, int *fd, t_env **envlst)
 			}
 			else
 			{
-				signal(SIGQUIT, &child_handler);
-				env = env_to_tab(*envlst); // A free, ca malloc + protection
-				cmdwpath = check_cmd(env, datalist->cmd); // A free
-				if (!cmdwpath)
-				{
-					free_tab(env);
-					exit (-1);
-				}
-				execve(cmdwpath, datalist->args, env);
+				exec_nobuiltin(datalist, envlst);
+				// signal(SIGQUIT, &child_handler);
+				// env = env_to_tab(*envlst); // A free, ca malloc + protection
+				// cmdwpath = check_cmd(env, datalist->cmd); // A free
+				// if (!cmdwpath)
+				// {
+				// 	free_tab(env);
+				// 	exit (-1);
+				// }
+				// execve(cmdwpath, datalist->args, env);
 				// perror(NULL);
 			}
 		}
@@ -130,9 +152,6 @@ int	exec_onepipe(t_datalist *datalist, int *fd, t_env **envlst)
 	return (0);
 }
 
-/*
-Ctrl-D marche plus depuis que j'ai fait ca...
-*/
 int	wait_processes(t_datalist *datalist)
 {
 	t_datalist	*tmp;
@@ -171,7 +190,7 @@ int	exec(t_big_list *list, t_env **envlst)
 	ft_bzero(fd, 4 * sizeof(int));
 	datalist = init_struct(list);
 	tmp = datalist;
-	// print_datalist(datalist);
+	//print_datalist(datalist);
 	while (tmp)
 	{
 		if (set_pipe(tmp, fd) < 0)
