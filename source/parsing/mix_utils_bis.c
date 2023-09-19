90ab58251c982b034fae8af7c6b06a77087f7f52
tmalidi <theomalidiorientation@gmail.com>
2023-09-19 16:13:55 +0200

    save

--- source/parsing/mix_utils_bis.c
+++ source/parsing/mix_utils_bis.c
@@ -6,7 +6,7 @@
 /*   By: tmalidi <tmalidi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/16 14:56:31 by tmalidi           #+#    #+#             */
-/*   Updated: 2023/09/19 13:08:40 by tmalidi          ###   ########.fr       */
+/*   Updated: 2023/09/19 15:57:45 by tmalidi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,24 +21,22 @@ char	*free_trim(char *str)
 
 int	potential_error(t_element	*etmp, t_big_list	*tmp)
 {
+	(void) tmp;
 	if ((etmp->type == 1 || etmp->type == 2 || etmp->type == 3
-			|| etmp->type == 4) && etmp->next == NULL)
+			|| etmp->type == 4))
 	{
-		if (tmp->next == NULL)
-		{
 			if (etmp->previous && (etmp->previous->type == 1 || etmp->previous->type == 2 || etmp->previous->type == 3
-					|| etmp->previous->type == 4) && etmp->next == NULL)
+					|| etmp->previous->type == 4))
+			{
+				printf("passsseee\n");
 				return (error_manager(etmp->str, SYNTAX),
-					g_return_value = 130, 0);	
-			else
+					g_return_value = 130, 0);
+			}
+			if (!etmp->next)
 			{
 				return (error_manager("\\n", SYNTAX),
 					g_return_value = 130, 0);
 			}
-		}
-		else
-			return (error_manager("|", SYNTAX),
-				g_return_value = 130, 0);
 	}
 	return (1);
 }
