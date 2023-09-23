b124dba01ae10771de63a211165993e2036026ad
Theo Malidi <tmalidi@z4r8p7.42lyon.fr>
2023-09-23 17:54:12 +0200

    8

--- source/parsing/expand_utils_bis.c
+++ source/parsing/expand_utils_bis.c
@@ -6,7 +6,7 @@
 /*   By: tmalidi <tmalidi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/16 14:46:36 by tmalidi           #+#    #+#             */
-/*   Updated: 2023/09/23 15:04:52 by tmalidi          ###   ########.fr       */
+/*   Updated: 2023/09/23 17:49:43 by tmalidi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,8 @@ char	*change(char *str, char *final, t_env *env, char *rv)
 	value = get_value_env(env, str + 1);
 	if (!ft_strncmp("$?", str, ft_strlen(str)))
 		return (rp_env(final, str, rv));
+	else if (ft_isdigit(str[1]))
+		return (rp_env(final, str, str + 2));
 	else if (!value)
 		return ("\0");
 	else
