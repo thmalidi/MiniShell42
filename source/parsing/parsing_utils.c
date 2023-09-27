eddd8e40bed5e4b0f9f212dc1eb091a3c6a173be
Theo Malidi <tmalidi@z4r7p7.42lyon.fr>
2023-09-27 13:00:48 +0200

    leaks ok pour de vrai cette fois

--- source/parsing/parsing_utils.c
+++ source/parsing/parsing_utils.c
@@ -6,7 +6,7 @@
 /*   By: tmalidi <tmalidi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/11 17:03:32 by tmalidi           #+#    #+#             */
-/*   Updated: 2023/09/27 12:28:24 by tmalidi          ###   ########.fr       */
+/*   Updated: 2023/09/27 12:59:08 by tmalidi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,8 +44,12 @@ int	pars_arg_op(char **tab, int i, t_env **envlst)
 	while (tab[i])
 	{
 		tab[i] = expand(tab[i], envlst);
-		if (!tab[i])
+		if (!tab[i++])
+		{
+			while (tab[i])
+				free(tab[i++]);
 			return (0);
+		}
 		i++;
 	}
 	return (1);
